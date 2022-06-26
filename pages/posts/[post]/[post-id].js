7567693bb51b30203ba9f6886a059bd7e5a3cc60
Abd Elhadi <abdelhadips2020@gmail.com>
2022-06-26 15:43:33 +0300

    first commit

---
+++ pages/posts/[post]/[post-id].js
@@ -0,0 +1,15 @@
+import Layout from "../../../components/layout";
+import Head from "next/head";
+
+export default function PostById() {
+  return (
+    <Layout>
+      <Head>
+        <title>Posts</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1></h1>
+    </Layout>
+  );
+}
